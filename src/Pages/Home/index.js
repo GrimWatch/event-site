38cf9db129ae639de6ba826a243227cb48a11c4f
Vishnu Vardhan JS <jaisrini76@gmail.com>
2024-03-20 18:53:33 +0530

    [Added] Alert for Postponing

--- src/Pages/Home/index.js
+++ src/Pages/Home/index.js
@@ -1,4 +1,5 @@
 import { useEffect } from "react";
+import Swal from 'sweetalert2';
 import DisplayEvent from "../../Components/DisplayEvent";
 import Banner from "../../Components/Banner";
 import Footer from "../../Components/Footer";
@@ -14,6 +15,15 @@ function Home() {
     useEffect(() => {
         window.scrollTo({ top: 0, left: 0, behavior: 'smooth' });
     }, [technical])
+
+    useEffect(() => {
+        Swal.fire({
+          title: 'Announcement!',
+          text: 'Symposium date have been changed from \r 03/04/2024 to 23/04/2024',
+          icon: 'warning',
+          confirmButtonText: 'OK'
+        });
+      }, []);
 
 
     return (
